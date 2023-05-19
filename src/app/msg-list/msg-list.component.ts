6ae0051272cd0cb6f2985f41adb0fff505cf14c1
Steven <b2977053@venus.lit.edu.tw>
2023-05-20 01:03:05 +0800

    在 msg-list 加入 mouseenter & mouseleave

--- src/app/msg-list/msg-list.component.ts
+++ src/app/msg-list/msg-list.component.ts
@@ -6,22 +6,36 @@ import { Status } from 'Services/Models/status';
 @Component({
   selector: 'app-msg-list',
   template: `
-  <div *ngFor="let item of _tasks.getList();let i = index">
+  <div *ngFor="let item of _tasks.getList();let i = index"
+  (mouseenter)="msgItemMouseEnter($event)"
+  (mouseleave)="msgItemMouseLeave($event)"
+  >
     {{item.msgContent}} - {{item.msgStatus}}
-    <input type="button" value="X" (click)="msgCancle(i)">
+    <input type="button" value="X" (click)="msgCancle(i)" style="display: none;">
   </div>
   `,
   styles: [
   ]
 })
-export class MsgListComponent implements OnInit  {
+export class MsgListComponent implements OnInit {
   _tasks = inject(TasksService);
 
   ngOnInit(): void {
 
   }
 
-  msgCancle(index: number){
+  msgCancle(index: number) {
     this._tasks.cancel(index);
+  }
+
+  msgItemMouseEnter($event: MouseEvent) {
+    let elDiv = ($event.target as HTMLInputElement);
+    let elInput = elDiv .getElementsByTagName('input')[0]
+    elInput.style.display = 'initial';
+  }
+  msgItemMouseLeave($event: MouseEvent) {
+    let elDiv = ($event.target as HTMLInputElement);
+    let elInput = elDiv .getElementsByTagName('input')[0]
+    elInput.style.display = 'none';
   }
 }
